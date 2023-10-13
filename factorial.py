76314ab847b7ea509bafe10e90973a48bf3c6567
usanaphtal112 <usanaphtal112@gmail.com>
2023-10-13 09:25:29 +0300

    Added Strong Number function and tests

--- factorial.py
+++ factorial.py
@@ -1,6 +1,8 @@
 """
-This module contains functions for calculating factorial.
+This module contains functions for calculating factorial,
+and also check strong Number
 """
+import math
 
 
 def calculate_factorial(input_number):
@@ -8,7 +10,7 @@ def calculate_factorial(input_number):
     Calculate the factorial of a number.
 
     Args:
-        n (int): The number for which to calculate the factorial.
+        input_number (int): The number for which to calculate the factorial.
 
     Returns:
         int: The factorial of the input number.
@@ -16,3 +18,24 @@ def calculate_factorial(input_number):
     if input_number == 0:
         return 1
     return input_number * calculate_factorial(input_number - 1)
+
+
+def check_strong(input_num):
+    """checking if the number is strong
+
+    Args:
+        input_num (int): the number to check weather it is
+        strong number or not.
+
+    Returns:
+        str: Strong Number or Not Strong Number.
+    """
+    copy_input = input_num
+    strong_num = 0
+    while input_num != 0:
+        remainder = input_num % 10
+        strong_num = strong_num + math.factorial(remainder)
+        input_num = input_num // 10
+    if copy_input == strong_num:
+        return "Strong Number"
+    return "Not Strong Number"
--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,7 +1,7 @@
 """
 Unit tests for the calculate_factorial function in the factorial module.
 """
-from factorial import calculate_factorial
+from factorial import calculate_factorial, check_strong
 
 
 def test_factorial():
@@ -15,3 +15,16 @@ def test_factorial():
     assert calculate_factorial(1) == 1
     assert calculate_factorial(5) == 120
     assert calculate_factorial(10) == 3628800
+
+
+def test_strong():
+    """Test the function that is checking
+    weather the number is strong or not
+    """
+
+    assert check_strong(145) == "Strong Number"
+    assert check_strong(122) == "Not Strong Number"
+    assert check_strong(1) == "Strong Number"
+    assert check_strong(40585) == "Strong Number"
+    assert check_strong(100000) == "Not Strong Number"
+    assert check_strong(0) == "Strong Number"
